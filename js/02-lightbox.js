3f3329d7ebc72d198bc81196150edaf6dd53b192
AnnaKulib <83002978+AnnaKulib@users.noreply.github.com>
2022-02-12 18:44:50 +0200

    task-02.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,17 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const listGalley = document.querySelector('.gallery');
+
+const itemOfGallery = galleryItems.map((item) =>
+  ` <li>
+        <a class="gallery__item" href=${item.original}>
+            <img class="gallery__image" src=${item.preview} alt=${item.description} />
+        </a>
+    </li>`)
+  .join(' ');
+
+  listGalley.insertAdjacentHTML('afterbegin', itemOfGallery);
+
+  var lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250,  captionsData: 'alt'});
