f0f5a5ab80a349e0c7c831df295890fcb5f00f3a
AnnaKulib <83002978+AnnaKulib@users.noreply.github.com>
2022-02-12 17:34:44 +0200

    task-01.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,70 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const listGalley = document.querySelector('.gallery');
+
+const itemOfGallery = galleryItems.map((item) =>
+  `<div class="gallery__item">
+        <a class="gallery__link" href=${item.original}>
+            <img
+                class="gallery__image"
+                src=${item.preview}
+                data-source=${item.original}
+                alt=${item.description}
+            />
+        </a>
+    </div>`)
+  .join(' ');
+
+  listGalley.insertAdjacentHTML('afterbegin', itemOfGallery);
+
+  listGalley.addEventListener('click', onListGalleryClick);
+
+function onListGalleryClick(event) {
+    event.preventDefault();
+
+    const currentEl = event.target;
+    const currentValue = currentEl.dataset.source;
+
+    if (!currentValue) {
+        return;
+    }
+    const instance = basicLightbox.create(
+        `<img src = ${currentValue} width="800" height="600">`
+    )
+        instance.show()
+    }
+
+    // -----------------------------------------------------------
+    // Вариант функции с добавление условий (закрытие по Esc)
+
+    // function onListGalleryClick(event) {
+    //     event.preventDefault();
+    
+    //     const currentEl = event.target;
+    //     const currentValue = currentEl.dataset.source;
+    
+    //     if (!currentValue) {
+    //         return;
+    //     }
+ 
+    //     function onEscPress(event) {
+    //         if (event.code === 'Escape') {
+    //           instance.close();
+    //         }
+    //       }
+    
+    //     const instance = basicLightbox.create(
+    //     `<img src = ${currentValue} width="800" height="600">`,
+    //      {
+    //         onShow: instance => {
+    //           window.addEventListener('keydown', onEscPress);
+    //         },
+    //         onClose: instance => {
+    //           window.removeEventListener('keydown', onEscPress);
+    //         },
+    //       }
+    // )
+    //     instance.show()
+    // }
